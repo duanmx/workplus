453ce0c6327cdf620086f7e7b72e1c5e36ff9d6f
duanmxc <duanmxcode@sina.com>
2015-11-02 15:45:42 +0800

    changes log

---
+++ appmarket/atwork-appmarket-bootstrap-2.0.0-GA2015092906/bin/stop.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cd `dirname $0`
+BIN_DIR=`pwd`
+cd ..
+DEPLOY_DIR=`pwd`
+ETC_DIR=$DEPLOY_DIR/etc
+
+PIDS=`ps  --no-heading -C java -f --width 1000 | grep "$ETC_DIR" |awk '{print $2}'`
+if [ -z "$PIDS" ]; then
+    echo "ERROR: appmarket does not started!"
+    exit 1
+fi
+
+echo -e "stopping appmarket ...\c"
+for PID in $PIDS ; do
+	kill $PID > /dev/null 2>&1
+done
+
+COUNT=0
+while [ $COUNT -lt 1 ]; do    
+    echo -e ".\c"
+    sleep 1
+    COUNT=1
+    for PID in $PIDS ; do
+		PID_EXIST=`ps --no-heading -p $PID`
+		if [ -n "$PID_EXIST" ]; then
+			COUNT=0
+			break
+		fi
+	done
+done
+echo "OK!"
+echo "PID: $PIDS"
