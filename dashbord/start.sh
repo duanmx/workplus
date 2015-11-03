1754d98c214506eb5eed3c3453b8b18eeb74ca53
duanmx <369216872@qq.com>
2015-11-03 11:04:10 +0800

    Update start.sh

--- dashbord/start.sh
+++ dashbord/start.sh
@@ -1,2 +1,2 @@
-cd $WEB_SER_PATH/$WEB_SER_NAME/webapps/$PROJECT_NAME/WEB-INF/classes && ./config.sh
-cd $WEB_SER_PATH/$WEB_SER_NAME/bin && ./catalina.sh run
+cd $WEB_SER_PATH/$WEB_SER_NAME/webapps/$PROJECT_NAME/WEB-INF/classes && chmod +x config.sh && ./config.sh
+cd $WEB_SER_PATH/$WEB_SER_NAME/bin && chmod +x catalina.sh && ./catalina.sh run
